547cd45599e73df2f76954e3e289863b159cf876
Yamuna Ashokan <yamunaashokan2000@gmail.com>
2025-07-06 19:10:22 +0530

    Update and rename Another_BFS to Another_BFS.java

---
+++ GRAPHS/Another_BFS.java
@@ -0,0 +1,6 @@
+/*"Another BFS" topic, which is based on edge weights 1 and 2, and learn how to convert it to 0-1 BFS format for efficient traversal.Problem Input:
+Undirected graph
+Edge weights: only 1 or 2
+You need to find the shortest distance from source C to destination D.
+Another BFS" = Single Source Shortest Path in 1/2 weighted graph
+using Deque to simulate greedy traversal without priority queue */
